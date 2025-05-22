04eec73a31bcdb98a24015d8cd171c4fb2015fbb
Renata Negrini <renatanegrinim@gmail.com>
2025-05-22 14:52:58 -0300

    Implementação do Spring Security e de Usuario, UsuarioRepository, UsuarioService e UsuarioController

---
+++ src/main/java/com/generation/blogpessoal/repository/UsuarioRepository.java
@@ -0,0 +1,16 @@
+package com.generation.blogpessoal.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.generation.blogpessoal.model.Usuario;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
+	
+	Optional<Usuario> findByUsuario(String usuario);
+	
+	Optional<Usuario> findAllByUsuarioIgnoreCase(String usuario);
+	
+	/* SELECT * FROM tb_usuarios WHERE usuario = ? */
+}
